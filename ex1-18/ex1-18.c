de67eca34cac80f0907ccd36996230816d289781
Răzvan Mocanu <razvan.mocanu@gmail.com>
2014-08-24 15:57:44 +0200

    Add solution for 1-18.

---
+++ ex1-18/ex1-18.c
@@ -0,0 +1,67 @@
+/* Exercise 1-18 from K+R
+ * Copyright (c) 2014 Răzvan Mocanu
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*
+ * Write a program to remove trailing blanks and tabs from each line of input,
+ * and to delete entirely blank lines.
+ */
+
+#include <stdio.h>
+
+#define MAX 5 /* Maximum input line length. */
+
+int getline(char s[], int lim);
+int trimline(char s[], int lim);
+
+int main()
+{
+	int len;
+	char line[MAX];
+
+	len = 0;
+	while ((len = getline(line, MAX)) > 0) {
+		if ((len = trimline(line, len)) > 0)
+			printf("%s\n", line);
+	}
+
+	return 0;
+}
+
+/* Reads a line into s and returns its length. */
+int getline(char s[], int lim)
+{
+	int c, i;
+
+	for (i = 0; i < lim - 1 && (c = getchar()) != EOF && c != '\n'; ++i)
+		s[i] = c;
+	if (c == '\n') {
+		s[i] = c;
+		++i;
+	}
+	s[i] = '\0';
+
+	return i;
+}
+
+/* Removes traling blanks and tabs from a line and returns its new length. */
+int trimline(char s[], int len) {
+	while (len > 0 &&
+			(s[len - 1] == ' ' || s[len - 1] == '\t' || s[len - 1] == '\n'))
+		--len;
+	s[len] = '\0';
+	return len;
+}
