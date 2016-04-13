0631cb214c2c116c0031a0041b82bb77fb2a323c
Răzvan Mocanu <razvan.mocanu@gmail.com>
2016-04-13 21:52:18 +0200

    CHANGE to Visual Studio 2015 solution.
    Update structure to VS2015 solution and projects. Remove old
    Makefiles for Cygwin and MinGW. Update .gitignore and
    .gitattributes for VS2015.

---
+++ Exercise 1-4/Exercise 1-4.c
@@ -0,0 +1,42 @@
+/*
+ * Exercise 1-4 from K+R
+ * Copyright (c) 2013 Răzvan Mocanu
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*
+ * Write a program to print the corresponding Celsius to Fahrenheit table.
+ */
+
+#include <stdio.h>
+
+main()
+{
+	double celsius, fahr, lower, upper, step;
+
+	lower = 0.0;
+	upper = 300.0;
+	step = 20.0;
+
+	/* Table heading */
+	printf("%2s %5s\n----------\n", "C", "F");
+
+	celsius = lower;
+	while (celsius <= upper) {
+		fahr = (9.0 / 5.0) * celsius + 32.0;
+		printf("%3.0f %6.1f\n", celsius, fahr);
+		celsius = celsius + step;
+	}
+}
