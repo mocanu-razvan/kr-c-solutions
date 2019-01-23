7891ec9db21ba0f5c7aea509434f1c71b96c45e5
Răzvan Mocanu <razvan.mocanu@gmail.com>
2019-01-24 00:56:11 +0100

    ADD improvements to solution for exercise 2-3.

--- Exercise 2-3/Exercise 2-3.c
+++ Exercise 2-3/Exercise 2-3.c
@@ -1,56 +1,79 @@
 ﻿/*
- * Exercise 2-3 from K+R
- * Copyright (c) 2017 Răzvan Mocanu
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+	Exercise 2-3 from K+R
+	Copyright (c) 2017 Răzvan Mocanu
+
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 /*
- * Write a function htoi(s), which converts a string of hexadecimal
- * digits (including an optional 0x or 0X) into its equivalent integer
- * value. The allowable digits are 0 through 9, a through f, and A
- * through F.
- */
+	Write a function htoi(s), which converts a string of hexadecimal
+	digits (including an optional 0x or 0X) into its equivalent integer
+	value. The allowable digits are 0 through 9, a through f, and A
+	through F.
+*/
 
 #include <ctype.h>
 #include <stdio.h>
 #include <string.h>
 
-int main() {}
+#define FALSE 0
+#define TRUE 1
 
-long htoi(const char s[]) {
+unsigned long htoi(const char[]);
+
+int main()
+{
+	printf("%16s (16) = %ld (10)\n", "0x07e3", htoi("0x07e3"));
+	printf("%16s (16) = %ld (10)\n", "0XFF", htoi("0XFF"));
+	printf("%16s (16) = %ld (10)\n", "07e3", htoi("07e3"));
+	printf("%16s (16) = %ld (10)\n", "0x07OK", htoi("0x07OK"));
+	printf("%16s (16) = %ld (10)\n", "X1234", htoi("X1234"));
+	printf("%16s (16) = %ld (10)\n", "0xffffffff00", htoi("0xffffffff00"));
+
+	return 0;
+}
+
+unsigned long htoi(const char s[])
+{
 	const unsigned int l = strlen(s);
-	unsigned int i, d;
-	long n;
+	unsigned int i;		/* Current index in input string. */
+	unsigned char d;	/* Integer value of current hexadecimal digit. */
+	unsigned long n;	/* Integer result of conversion. */
+	char h;				/* Flag to indicate if digit is hexadecimal. */
 
 	i = 0;
 	if (l > 1 && s[0] == '0' && (s[1] == 'x' || s[1] == 'X'))
 		i = 2;
 
 	n = 0;
-	while (i < l) {
-		if (s[i] >= '0' && s[i] <= '9')
+	h = TRUE;
+	for (; i < l && h; ++i) {
+		if (isdigit(s[i]))
 			d = s[i] - '0';
 		else if (s[i] >= 'a' && s[i] <= 'f')
 			d = s[i] - 'a' + 10;
 		else if (s[i] >= 'A' && s[i] <= 'F')
 			d = s[i] - 'A' + 10;
 		else
-			return -1;
-		n = n * 16 + d;
-		++i;
+			h = FALSE;
+		if (h)
+			n = n * 16 + d;
 	}
 
+	/*
+		Depending on the length of s, n might overflow.
+		In this case, the result will be incorrect.
+	*/
 	return n;
 }
