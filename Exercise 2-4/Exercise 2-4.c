751a4b2ef22c7d259ab1833754825eacf6c1da6c
Răzvan Mocanu <razvan.mocanu@gmail.com>
2019-01-24 22:05:53 +0100

    ADD solution for exercise 2-4.

---
+++ Exercise 2-4/Exercise 2-4.c
@@ -0,0 +1,22 @@
+﻿/*
+	Exercise 2-4 from K+R
+	Copyright (c) 2019 Răzvan Mocanu
+
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/*
+	Write an alternative version of squeeze(s1, s2) that deletes each
+	character in s1 that matches any character in the string s2.
+*/
