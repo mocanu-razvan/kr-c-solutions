fce9f7d9631f9ea94d06b31441d1c48f7058216a
Răzvan Mocanu <razvan.mocanu@gmail.com>
2019-02-16 10:34:17 +0100

    Format tests for exercise 2-3.

--- Exercise 2-3/Exercise 2-3.c
+++ Exercise 2-3/Exercise 2-3.c
@@ -1,26 +1,26 @@
 ﻿/*
-	Exercise 2-3 from K+R
-	Copyright (c) 2019 Răzvan Mocanu
+Exercise 2-3 from K+R
+Copyright (c) 2019 Răzvan Mocanu
 
-	This program is free software: you can redistribute it and/or modify
-	it under the terms of the GNU General Public License as published by
-	the Free Software Foundation, either version 3 of the License, or
-	(at your option) any later version.
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
 
-	This program is distributed in the hope that it will be useful,
-	but WITHOUT ANY WARRANTY; without even the implied warranty of
-	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-	GNU General Public License for more details.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
 
-	You should have received a copy of the GNU General Public License
-	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 /*
-	Write a function htoi(s), which converts a string of hexadecimal
-	digits (including an optional 0x or 0X) into its equivalent integer
-	value. The allowable digits are 0 through 9, a through f, and A
-	through F.
+Write a function htoi(s), which converts a string of hexadecimal
+digits (including an optional 0x or 0X) into its equivalent integer
+value. The allowable digits are 0 through 9, a through f, and A
+through F.
 */
 
 #include <ctype.h>
@@ -34,12 +34,14 @@ unsigned long htoi(const char[]);
 
 int main()
 {
-	printf("%16s (16) = %ld (10)\n", "0x07e3", htoi("0x07e3"));
-	printf("%16s (16) = %ld (10)\n", "0XFF", htoi("0XFF"));
-	printf("%16s (16) = %ld (10)\n", "07e3", htoi("07e3"));
-	printf("%16s (16) = %ld (10)\n", "0x07OK", htoi("0x07OK"));
-	printf("%16s (16) = %ld (10)\n", "X1234", htoi("X1234"));
-	printf("%16s (16) = %ld (10)\n", "0xffffffff00", htoi("0xffffffff00"));
+	printf("%s\t\t%s\t%s\n", "Test", "Expected", "Actual");
+	printf("%s\t\t%s\t%s\n", "----", "--------", "------");
+	printf("%s\t\t%ld\t\t%ld\n", "0x07e3", 2019L, htoi("0x07e3"));
+	printf("%s\t\t%ld\t\t%ld\n", "0XFF", 255L, htoi("0XFF"));
+	printf("%s\t\t%ld\t\t%ld\n", "07e3", 2019L, htoi("07e3"));
+	printf("%s\t\t%ld\t\t%ld\n", "0x07OK", 7L, htoi("0x07OK"));
+	printf("%s\t\t%ld\t\t%ld\n", "X1234", 0L, htoi("X1234"));
+	printf("%s\t%ld\t\t%ld\n", "0xffffffff00", -256L, htoi("0xffffffff00"));
 
 	return 0;
 }
