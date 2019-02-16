12c6dbfe923ccd1f76f5446cd8819d7a91d5d5ef
Răzvan Mocanu <razvan.mocanu@gmail.com>
2019-02-16 10:48:15 +0100

    Fix comment style.

--- Exercise 1-10/Exercise 1-10.c
+++ Exercise 1-10/Exercise 1-10.c
@@ -1,26 +1,26 @@
 /*
- * Exercise 1-10 from K+R
- * Copyright (c) 2019 Răzvan Mocanu
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
+Exercise 1-10 from K+R
+Copyright (c) 2019 Răzvan Mocanu
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 /*
- * Write a program to copy its input to its output, replacing each tab by \t,
- * each backspace by \b, and each backslash by \\. This makes tabs and
- * backspaces visible in an unambiguous way.
- */
+Write a program to copy its input to its output, replacing each tab by \t,
+each backspace by \b, and each backslash by \\. This makes tabs and
+backspaces visible in an unambiguous way.
+*/
 
 #include <stdio.h>
 
